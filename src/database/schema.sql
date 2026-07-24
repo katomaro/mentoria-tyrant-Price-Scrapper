53a58d7a0bce772d44dcb68928e555e9326a65f1
Arthur Kayky Carvalho Silva <arthlokobr@gmail.com>
2026-07-23 23:22:43 -0300

    feat(schema): ensure table does not get overwritten

--- src/database/schema.sql
+++ src/database/schema.sql
@@ -1,11 +1,11 @@
-CREATE TABLE products (
+CREATE TABLE IF NOT EXISTS products (
     product_id INTEGER PRIMARY KEY AUTOINCREMENT,
     product_name VARCHAR(256) NOT NULL,
     product_price DECIMAL(10,2) NOT NULL,
     product_url VARCHAR(512) NOT NULL
 );
 
-CREATE TABLE product_categories (
+CREATE TABLE IF NOT EXISTS product_categories (
     product_model VARCHAR(256) NOT NULL,
     product_sku VARCHAR(256) NOT NULL,
     product_manufacturer VARCHAR(256) NOT NULL,
